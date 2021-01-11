eb60646a3fb9c2be660aac02e457b9390e9ae1fe
RAJENDRA GUPTA <62395429+rajendragupta728@users.noreply.github.com>
2021-01-11 08:45:36 +0530

    donetvtask

---
+++ pic1.js
@@ -0,0 +1,224 @@
+document.querySelector(".btn-roll1").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 12.jpg";
+   for(var i =1;i<=12;i++){
+    var b= document.querySelector(".btn-roll"+i)
+       if(i== 1){
+        b.style.backgroundColor = "purple"
+        b.id="active"
+       }
+       else{
+        b.style.backgroundColor = ""
+        b.id="bg"
+       }
+   }
+   document.getElementById("new").style.backgroundColor="purple"
+   document.getElementById("new1").style.backgroundColor="purple"
+  });
+document.querySelector(".btn-roll2").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 3.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 2){
+            b.style.backgroundColor = "blue"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }     
+       }
+       document.getElementById("new").style.backgroundColor="blue"
+       document.getElementById("new1").style.backgroundColor="blue"
+  });
+  document.querySelector(".btn-roll3").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 9.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 3){
+            b.style.backgroundColor = " grey"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="grey"
+       document.getElementById("new1").style.backgroundColor="grey"
+  });
+  document.querySelector(".btn-roll4").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 11.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 4){
+            b.style.backgroundColor = "#b0b6bf "
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#b0b6bf"
+       document.getElementById("new1").style.backgroundColor="#b0b6bf"
+  });
+  document.querySelector(".btn-roll5").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 8.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 5){
+            b.style.backgroundColor = "#eda155"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#eda155"
+       document.getElementById("new1").style.backgroundColor="#eda155"
+  });
+document.querySelector(".btn-roll6").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Virgo.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 6){
+            b.style.backgroundColor = "#452b17"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="black"
+       document.getElementById("new1").style.backgroundColor="black"
+  });
+  document.querySelector(".btn-roll7").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 7.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 7){
+            b.style.backgroundColor = "#07851a"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#07851a"
+       document.getElementById("new1").style.backgroundColor="#07851a"
+  });
+  document.querySelector(".btn-roll8").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 4.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 8){
+            b.style.backgroundColor = "#f0b429"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#f0b429"
+       document.getElementById("new1").style.backgroundColor="#f0b429"
+  });
+  document.querySelector(".btn-roll9").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 5.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 9){
+            b.style.backgroundColor = "#99de21"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#99de21"
+       document.getElementById("new1").style.backgroundColor="#99de21"
+  });
+document.querySelector(".btn-roll10").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 10.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 10){
+            b.style.backgroundColor = "#1d043d"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#1d043d"
+       document.getElementById("new1").style.backgroundColor="#1d043d"
+  });
+  document.querySelector(".btn-roll11").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 13.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 11){
+            b.style.backgroundColor = "#07ede2"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#07ede2"
+       document.getElementById("new1").style.backgroundColor="#07ede2"
+  });
+  document.querySelector(".btn-roll12").addEventListener("click", function () {
+    var diceDOM = document.querySelector(".Pic");
+    diceDOM.src = "Layer 6.jpg";
+    for(var i =1;i<=12;i++){
+        var b= document.querySelector(".btn-roll"+i)
+           if(i== 12){
+            b.style.backgroundColor = "#76d68c"
+            b.id="active"
+           }
+           else{
+            b.style.backgroundColor = ""
+            b.id="bg"
+           }
+          
+          
+       }
+       document.getElementById("new").style.backgroundColor="#76d68c"
+       document.getElementById("new1").style.backgroundColor="#76d68c"
+  });
